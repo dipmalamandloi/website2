c78607022e647f3142924317d24406be85a6d269
dipmalamandloi <dipmalamandloi@gmail.com>
2019-12-28 16:53:09 +0530

    commit

---
+++ website11.com/src/main/java/website11/com/Application.java
@@ -0,0 +1,10 @@
+package website11.com;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello WOrld");
+        System.out.println("Hello India");	
+	}
+
+}
